03dfe621353f341c4855c3a0954fd4d33ac660e5
CARINE MENDES FERREIRA <20272546@cfp127aluno.local>
2021-02-08 11:16:10 -0300

    Aula 08/02/2021

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/36/00c60562fd69001b15d1f7db2c88f618
@@ -0,0 +1,14 @@
+package br.com.empresa.Academico.model;
+
+import java.time.LocalDate;
+
+public class Projetoacademico {
+	
+	private String name;
+	private LocalDate datanascimento;
+	private char sexo;
+	private double peso; 
+	private  double altura;
+	private int niveldeatividade;
+	
+}
---
+++ Academico/src/br/com/empresa/Academico/model/NivelAtividade.java
@@ -0,0 +1,15 @@
+package br.com.empresa.Academico.model;
+
+public class NivelAtividade {
+	
+//	Mulheres
+	public static final String LEVE = ("1.6");
+	public static final String MODERADO  = ("1.6");
+	public static final String INTENSO = ("1.8");
+
+// HOMENS 
+	public static final String LEVE1 = ("1.5");
+	public static final String MODERADO1  = ("1.8");
+	public static final String INTENSO1 = ("2.1");
+	
+}
